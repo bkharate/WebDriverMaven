5cc2a7b36b2c8a63f4dede9dcb021cae2c4567a6
BHAGVAT <bhagvat.kharate89@gmail.com>
2017-12-12 11:18:31 +0530

    my second commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,39 @@
+package webdriverMaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp (){
+		
+		System.setProperty("webdriver.chrome.driver","E:\\java\\Jar files\\chromedriver.exe");
+		
+		 driver = new ChromeDriver();
+	}
+	
+	
+	@Test
+	public void dologin(){
+		
+		driver.get("https://en-gb.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(40l, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("ninad.krishna");
+		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("7058063437");
+	}
+	
+	@AfterTest
+	public void teardown(){
+		driver.quit();
+	}
+	
+
+}
